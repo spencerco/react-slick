eaae46052b574e8427dbc38aa252ee1ba78d0736
Wouter Landuydt <wouter.landuydt@spencer.co>
2018-12-03 15:38:02 +0100

    can go next if in center mode

--- src/utils/innerSliderUtils.js
+++ src/utils/innerSliderUtils.js
@@ -78,9 +78,9 @@ export const canGoNext = spec => {
   if (!spec.infinite) {
     if (spec.centerMode && spec.currentSlide >= spec.slideCount - 1) {
       canGo = false;
-    } else if (
+    } else if (!spec.centerMode && (
       spec.slideCount <= spec.slidesToShow ||
-      spec.currentSlide >= spec.slideCount - spec.slidesToShow
+      spec.currentSlide >= spec.slideCount - spec.slidesToShow)
     ) {
       canGo = false;
     }
